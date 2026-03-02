97b2c32c62568614ad7912d8a6f013ab518d64e8
arinjaysalgia <arinjaysalgia2006@gmail.com>
2026-03-02 14:59:43 +0530

    removing unused dependency.

--- backend/src/main/java/com/example/service/FoodItemService.java
+++ backend/src/main/java/com/example/service/FoodItemService.java
@@ -8,7 +8,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
